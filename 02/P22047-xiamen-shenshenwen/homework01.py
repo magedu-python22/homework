38a9058aa617ef6e0acb4af65f122533fa33e93f
Shen ShenWen <shensw3@qq.com>
2019-06-03 21:08:48 +0800

    第二周作业

---
+++ 02/P22047-xiamen-shenshenwen/homework01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import random
+
+result = int(str(random.randrange(0, 1000)).lstrip('0'))
+num = int(input('Please enter a number: ').strip().lstrip('0'))
+
+while not num == result:
+    if num > result:
+        print('猜错啦，比答案大哦')
+    if num < result:
+        print('猜错啦，比答案小哦')
+    num = int(input('Please enter a number: ').strip().lstrip('0'))
+
+print(f"\n恭喜，猜中啦!!! 答案是：{result}")
