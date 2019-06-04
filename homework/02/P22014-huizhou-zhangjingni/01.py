d9cd6ce9baaf5a62cd648c2d6199e0f7237ff5b5
mikii2019 <2019mikii2019@users.noreply.github.com>
2019-06-04 20:06:51 +0800

    第二周作业

--- homework/02/P22014-huizhou-zhangjingni/01.py
+++ homework/02/P22014-huizhou-zhangjingni/01.py
@@ -4,7 +4,7 @@
 # Date : 2019/6/3 19:54
 
 import random
-answer = random.randint(0,99)
+answer = random.randint(0,999)
 # print(answer)
 while True:
     n = input('plz input an integer:')
@@ -18,17 +18,3 @@ while True:
     flag = '>' if n > answer else '<'
     print('n {} answer'.format(flag) )
 
-
-# FizzBuzz
-dest = (0,'Fizz', 'Buzz', 'FizzBuzz')
-for i in range(101):
-    flag = 0
-    if i > 0:
-        if i % 3 == 0:
-            flag += 1
-        if i % 5 == 0:
-            flag += 2
-    if not flag:
-        print(i)
-    else:
-        print(dest[flag])
