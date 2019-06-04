35411b3924780ad1d9e01c63a85fea3f5352f9eb
xiaogejie <32904622@qq.com>
2019-06-04 14:16:25 +0800

    第二周作业

---
+++ homework/02/P22048-fuzhou-xiaojie/homework2-1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/4 0:01
+# @Author  : xiaojie
+# @Email   : 32904622@qq.com
+# @File    : homework2-1.py
+# @Software: PyCharm
+
+
+#  猜数字
+import random
+
+answer = random.randint(1, 100)
+for i in range(100):
+    num = int(input('输入一个小于100的数：'))
+    if num != answer:
+        if num > answer:
+            print('比答案大了一点')
+        else:
+            print('比答案小了一点')
+    else:
+        print('答对了')
+        break
---
+++ homework/02/P22048-fuzhou-xiaojie/homework2-2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/4 0:17
+# @Author  : xiaojie
+# @Email   : 32904622@qq.com
+# @File    : homework2-2.py
+# @Software: PyCharm
+
+
+#FizzBuzz
+
+for i in range(0, 101):
+    if i == 0:
+        print(i)
+    elif i % 3 == 0:
+        if i % 5 == 0:
+            print('FizzBuzz')
+        else:
+            print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
