7dcdfabc5dfacff7ac6f5136d44cc1a129e257d6
xiaogejie <32904622@qq.com>
2019-06-04 21:58:51 +0800

    第二周作业

--- homework/02/P22048-fuzhou-xiaojie/homework2-1.py
+++ homework/02/P22048-fuzhou-xiaojie/homework2-1.py
@@ -12,7 +12,7 @@ import random
 
 answer = random.randint(1, 1000)
 for i in range(1000):
-    num = int(input('输入一个小于100的数：'))
+    num = int(input('输入一个小于1000的数：'))
     if num != answer:
         if num > answer:
             print('比答案大了一点')
