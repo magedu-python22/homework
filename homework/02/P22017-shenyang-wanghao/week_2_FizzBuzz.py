02cb6b61c15e328314b907a73d2a6022f2a0a14d
Oscar Wang <wanghao_hit@126.com>
2019-06-06 13:48:54 +0800

    Submit week2's homework
    Signed-off-by: Oscar Wang <wanghao_hit@126.com>

---
+++ homework/02/P22017-shenyang-wanghao/week_2_FizzBuzz.py
@@ -0,0 +1,12 @@
+
+for number in range(0, 101, 1):
+	if int(number) % 15 == 0 and int(number) > 0:
+		number = 'FizzBuzz'
+	elif int(number) % 3 == 0 and int(number) > 0:
+		number = 'Fizz'
+	elif number % 5 == 0 and int(number) > 0:
+		number = 'Buzz'
+	
+	print(number)
+
+	
