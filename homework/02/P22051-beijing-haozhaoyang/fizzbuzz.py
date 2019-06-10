e1bd356ba5f77bfb8f8c2c9dd5b102e666c4a4e3
kevinhao <kevinhao@alaliyun.com>
2019-06-10 11:07:04 +0800

    remove  newline

--- homework/02/P22051-beijing-haozhaoyang/fizzbuzz.py
+++ homework/02/P22051-beijing-haozhaoyang/fizzbuzz.py
@@ -3,15 +3,15 @@
 
 for i in range(0,101):
    if i == 0:
-       print(i)
+       print(i, end=' ')
    elif i % 15 == 0:
    # if i % 3 == 0 and i % 5 == 0:
-       print('FizzBuzz')
+       print('FizzBuzz', end=' ')
    elif i % 3 == 0:
-       print('Fizz')
+       print('Fizz', end=' ')
    elif i % 5 == 0:
-       print('Buzz')
+       print('Buzz', end=' ')
    else:
-       print(i)
+       print(i, end=' ')
 
 # 不用换行输出试试？
