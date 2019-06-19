28d1d1598e0d2b0fdfb114fcfe34273789e29bc4
mikii2019 <2019mikii2019@users.noreply.github.com>
2019-06-19 10:40:22 +0800

    week 4 - sorting

---
+++ homework/04/P22014-huizhou-zhangjingni/week4-sorting.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author:Miki
+# Date : 2019/6/19 10:19
+
+# 给定一个只包含大小写字母，数字的字符串，对其进行排序，保证：
+# 1 所有的小写字母在大写字母前面
+# 2 所有的字母在数字前面
+# 3 所有的奇数在偶数前面
+
+s = "Sorting1234"
+lower = []
+upper = []
+odd = []
+even = []
+for c in s:
+    if c.islower():
+        lower.append(c)
+    elif c.isupper():
+        upper.append(c)
+    elif c.isdigit():
+        if int(c) & 1 == 1:
+            odd.append(c)
+        else:
+            even.append(c)
+
+dest = ''.join(sorted(lower) + sorted(upper) + sorted(odd) + sorted(even))
+print(dest)
+
