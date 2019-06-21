79e07796004577138297e877f942150dc71ebfb8
lennonyang368 <yangfeng201780@163.com>
2019-06-22 01:42:02 +0800

    第四周作业提交

---
+++ homework/04/P22044-hangzhou-yangfeng/4thWeek_Sort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[74]:
+
+
+s = "Sorting123456879AFLdfkgs"
+
+c = []
+bigc = []
+ns = []
+nd = []
+
+length = int(len(s))
+for i in range(length):
+    if s[i].isalpha() == True:
+        if s[i].islower() == True:
+            c.append(s[i])
+        else:
+            bigc.append(s[i])
+    else:
+        if s[i].isdigit() == True:
+            if (int(s[i]) % 2) == 0:
+                nd.append(s[i]) 
+            else: 
+                ns.append(s[i])
+print(c,bigc,ns,nd)
+
+newlist = [*c,*bigc,*ns,*nd]
+print(newlist)
+
