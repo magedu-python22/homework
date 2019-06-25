3aa492d7d07c89579d516504002e4ff2755615fb
Shen ShenWen <shensw3@qq.com>
2019-06-25 19:47:45 +0800

    使用函数

--- homework/05/P22047-xiamen-shenshenwen/PIN_v2.py
+++ homework/05/P22047-xiamen-shenshenwen/PIN_v2.py
@@ -1,5 +1,4 @@
 """
-【二：本周作业】请同学们于下周天晚上10点前，将作业上传至GitHub；
 随机生成100个验证码，每个验证码由6个元素构成:
 1、要求使用random模块
 2、验证码由字母和数字构成
@@ -14,19 +13,20 @@ T (202, 19, 72)
 F (182, 136, 237)
 """
 
+# 使用函数与列表解析式
+
 import random
 import string
 
-lst1 = []
-lst2 = []
+def tmp_PIN():
+    return random.sample(string.ascii_letters + string.digits, 6)
+
+def color():
+    return [random.randint(0, 255) for _ in range(3)]
+
+def seq():
+    return [color() for _ in range(len(tmp_PIN()))]
+
 for _ in range(100):
-    tmp_PIN = random.sample(string.ascii_letters + string.digits, 6)
-    for _ in range((len(tmp_PIN))):
-        for i in range(3):
-            lst1.append(random.randint(0, 255))
-        lst2.append(lst1)
-        lst1 = []
-    if len(lst2) == len(tmp_PIN):
-        PIN = dict(zip(tmp_PIN, lst2))
-        lst2 = []
+    PIN = dict(zip(tmp_PIN(), seq()))
     print(PIN)
