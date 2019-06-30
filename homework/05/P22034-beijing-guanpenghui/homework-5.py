9f0250e43ca52feea03e66d0f589c17dc680882e
关鹏辉 <690336596@qq.com>
2019-06-30 08:43:26 +0800

    homework-5

---
+++ homework/05/P22034-beijing-guanpenghui/homework-5.py
@@ -0,0 +1,21 @@
+import string
+import random
+strall = string.ascii_letters + string.digits
+vercode = []
+#vercol = {}
+for y in range(5):
+    x = random.choice(strall) +  random.choice(strall) +  random.choice(strall) +  random.choice(strall) +  random.choice(strall) +  random.choice(strall)
+    vercode.append([x])
+print('生成的验证码如下：',vercode)
+print()
+for str1 in vercode:
+    print('验证码{}中每个元素的颜色'.format(str1))
+    b = str1[0]
+    for c in b:
+        color = []
+        print(c,end=' ')
+        for d in range(3):
+            color.append(random.randint(0,255))
+        print(color)
+    print()
+
