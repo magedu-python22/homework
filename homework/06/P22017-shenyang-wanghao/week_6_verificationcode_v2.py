0ee0906b4ed334bae442cbc9126dea0c5402e59b
Oscar Wang <wanghao_hit@126.com>
2019-07-01 20:30:54 +0800

    Homework of Week 6

---
+++ homework/06/P22017-shenyang-wanghao/week_6_verificationcode_v2.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import random
+for j in range(100):
+	def getRandomColor():
+		"""get 6 random color with (r,g,b) format"""
+
+		color=(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+		return color
+
+	def getRandomStr():
+		"""get 6 random string"""
+
+		char = random.choice([chr(random.randint(65,90)), str(random.randint(0,9))])
+		return char
+		
+	image = Image.new('RGB', (180, 30),getRandomColor())
+	draw = ImageDraw.Draw(image)
+	font = ImageFont.truetype("kumo.ttf",size=26)
+
+	for i in range(6):
+
+		random_char = getRandomStr()
+		draw.text((10+i*30,0),random_char,getRandomColor(),font=font)
+	image.save(open(str(j)+".png",'wb'),'png')
+
+
+	
+		
