42ff3b1b297338fa90f9a271ab273ff7111e1c86
Shen ShenWen <shensw3@qq.com>
2019-07-01 22:16:49 +0800

    some change

--- homework/06/P22047-xiamen-shenshenwen/PIN_convert.py
+++ homework/06/P22047-xiamen-shenshenwen/PIN_convert.py
@@ -9,31 +9,31 @@ def PIN():
 def color():
     return (random.randint(0, 255) for _ in range(3))
 
-# 设置图片大小，以像素为单位
-w, h = 360, 65
+def PIN_convert(mode='RGBA', width=360, height=65):
+    # 构建image对象,(色彩类型，size(二元组)，color)
+    image = Image.new(mode, (width, height), tuple(color()))
 
-# 构建image对象,(mode，size(二元组)，color)
-image = Image.new('RGBA', (w, h), tuple(color()))
+    # 加载自定义字体，并创建字体对象
+    font = ImageFont.truetype('fonts/COOPBL.TTF', size=40)
 
-# 加载自定义字体，并创建字体对象
-font = ImageFont.truetype('fonts/COOPBL.TTF', size=40)
+    # 创建draw对象
+    draw = ImageDraw.Draw(image, mode)
 
-# 创建draw对象
-draw = ImageDraw.Draw(image, 'RGBA')
+    # 填充背景色
+    for x in range(width):
+        for y in range(height):
+            draw.point((x, y), fill=(225, 255, 255))
 
-# 填充背景色
-for x in range(w):
-    for y in range(h):
-        draw.point((x, y), fill=(225, 255, 255))
+    for code in range(6):
+        draw.text((60*code+10, random.randint(-10, 20)), PIN(), font=font, fill=tuple(color()))
+    """
+    显示验证码，并偏移显示位置
+    不会算法，偏移位置瞎整的，不超界就行了
+    """
+    # 返回高斯模糊处理
+    return image.filter(ImageFilter.GaussianBlur())
 
-"""
-显示验证码，并偏移显示位置
-不会算法，偏移位置瞎整的，不超界就行了
-"""
-for code in range(6):
-    draw.text((60*code+10, random.randint(-10, 20)), PIN(), font=font, fill=tuple(color()))
-
-# 高斯模糊处理
-image = image.filter(ImageFilter.GaussianBlur(2))
-# 保存图片
-image.save('PIN.png', 'png')
+for n in range(1,100):
+    # 保存图片
+    f = f'images/PIN_{n:0>3d}.png'
+    PIN_convert().save(f, 'png')
