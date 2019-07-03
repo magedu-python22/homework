fbc72d23080cb8779a1096a11e407d982c085e9c
jamhuang0722 <50984161+jamhuang0722@users.noreply.github.com>
2019-07-04 00:46:53 +0800

    Update random_code.py
    第六周作业

--- homework/06/P22041-shanghai-huanghao/random_code.py
+++ homework/06/P22041-shanghai-huanghao/random_code.py
@@ -9,14 +9,15 @@ def randcode():
 	strs = string.ascii_uppercase + string.digits
 	return random.choice(strs)
 
-width = 60 * 6
-height = 60
-image = Image.new('RGB', (width, height), (0, 0, 0))
-draw = ImageDraw.Draw(image)
 font = ImageFont.truetype('C:/Windows/Fonts/ALGER.TTF', 36)
-for x in range(width):
-	for y in range(height):
-		draw.point((x, y), fill=randcolor(50, 80))
-for t in range(6):
-	draw.text((60 * t + 10, 10), randcode(), font=font, fill=randcolor())
-image.save('code.jpg', 'jpeg')
+for i in range(100):
+	image = Image.new('RGB', (360, 60), (20, 20, 20))
+	draw = ImageDraw.Draw(image)
+	''' 背景颜色处理，比较耗时
+	for x in range(360):
+		for y in range(60):
+			draw.point((x, y), fill=randcolor(50, 80))
+	'''
+	for t in range(6):
+		draw.text((60 * t + 10, 10), randcode(), font=font, fill=randcolor())
+	image.save(f'code{i+1}.jpg', 'jpeg')
