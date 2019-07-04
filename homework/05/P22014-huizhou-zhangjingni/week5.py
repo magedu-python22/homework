f7394cf70bee726a6f9e154883179dccb2a4e328
mikii2019 <2019mikii2019@users.noreply.github.com>
2019-07-04 10:44:15 +0800

    week5

--- homework/05/P22014-huizhou-zhangjingni/week5.py
+++ homework/05/P22014-huizhou-zhangjingni/week5.py
@@ -18,8 +18,6 @@ F (182, 136, 237)
 """
 import random
 import string
-import platform
-from PIL import Image, ImageDraw, ImageFont
 
 
 # 生成随机字符 --> string
@@ -32,26 +30,5 @@ def color():
     return tuple(random.randint(0,255) for _ in range(3))
 
 
-def create_image(n=1,code=6):
-    width = 30*code + 20
-    height = 30*n
-    image1 = Image.new('RGB', (width, height), )
-    draw1 = ImageDraw.Draw(image1)
-    # 不同操作系统字体路径不同
-    os = platform.system()
-    if os == 'Windows':
-        font = ImageFont.truetype(r'C:\Windows\Fonts\arial.ttf', size=26)
-    elif os == 'Linux':
-        font = ImageFont.truetype('/usr/share/fonts/arial.ttf', size=26)
-    else:
-        font = ImageFont.truetype('/Library/Fonts/Arial.ttf', size=26)
-    for i in range(n):
-        for j in range(code):
-            draw1.text((15 + j * 30, 30*i), vcode(), color(), font=font)
-    image1.show()
-    image1.save('code_image.jpg', 'jpeg')
-
-
-create_image(100)
-
-
+for _ in range(100):
+    print(tuple((vcode(),color()) for _ in range(6)))
