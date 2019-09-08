bed2ac42d3b880977cd201c1978e6327321762cc
moonsu <729185501@..com>
2019-09-08 09:51:08 +0800

    第十一周作业

---
+++ homework/11/P22045-shandong-liubin/带参装饰器.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from functools import wraps
+import time
+
+
+def decorator(times: int):
+    def _decorator(fn):
+        @wraps(fn)
+        def wrap(*args, **kwargs):
+            start = datetime.now()
+            ret = fn(*args, **kwargs)
+            datel = (datetime.now() - start).total_seconds()
+            if datel > times:
+                print("names:{}, {}s".format(fn.__name__, datel))
+            return ret
+
+        return wrap
+
+    return _decorator
+
+
+@decorator(1)
+def add(x, y):
+    time.sleep(2)
+    return print(x + y)
+
+
+add(4, 5)
---
+++ homework/11/P22045-shandong-liubin/无参装饰器.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from functools import wraps
+import time
+
+
+def decorator(fn):
+    @wraps(fn)
+    def wrap(*args, **kwargs):
+        start = datetime.now()
+        ret = fn(*args, **kwargs)
+        datel = (datetime.now() - start).total_seconds()
+        print("names:{}, {}s".format(fn.__name__, datel))
+        return ret
+
+    return wrap
+
+
+@decorator
+def add(x, y):
+    time.sleep(1)
+    return print(x + y)
+
+
+add(4, 5)
